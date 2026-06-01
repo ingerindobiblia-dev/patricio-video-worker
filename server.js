9e5083cfea9f93e52590ce84d69539f6787ef562
ingerindobiblia-dev <ingerindobiblia@gmail.com>
2026-06-01 20:26:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Patricio Video Worker está rodando!');
+  res.send('Patricio Video Worker está online e funcionando!');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Worker rodando na porta ${PORT}`);
 });
